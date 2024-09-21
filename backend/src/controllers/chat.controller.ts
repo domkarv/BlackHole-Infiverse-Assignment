ad994882469328a9079f9ce5d71a2ea5021f0bc8
Omkar Date <omkardate29@gmail.com>
2024-09-21 14:40:26 +0530

    chat bot with context

--- backend/src/controllers/chat.controller.ts
+++ backend/src/controllers/chat.controller.ts
@@ -2,14 +2,14 @@ import type { Request, Response } from "express";
 import { generateChatResponce } from "../lib/chat";
 
 export const chatController = async (req: Request, res: Response) => {
-  const { prompt } = await req.body;
+  const { prompt, history } = await req.body;
 
   if (!prompt) {
     return res.status(400).json({ error: "Prompt is required" });
   }
 
   try {
-    const response = await generateChatResponce({ prompt });
+    const response = await generateChatResponce({ prompt, history });
     return res.status(200).json({ success: true, text: response });
   } catch (error) {
     return res
--- backend/src/lib/chat.ts
+++ backend/src/lib/chat.ts
@@ -1,22 +1,48 @@
-import { createGoogleGenerativeAI } from "@ai-sdk/google";
-import { generateText } from "ai";
+import {
+  GoogleGenerativeAI,
+  HarmBlockThreshold,
+  HarmCategory,
+} from "@google/generative-ai";
 
-const model = createGoogleGenerativeAI({
-  apiKey: process.env.GEMINI_API_KEY,
-});
-
-export async function generateChatResponce({ prompt }: { prompt: string }) {
-  const { text } = await generateText({
-    model: model("models/gemini-1.5-flash-latest", {
-      safetySettings: [
-        {
-          category: "HARM_CATEGORY_HATE_SPEECH",
-          threshold: "BLOCK_LOW_AND_ABOVE",
-        },
-      ],
-    }),
-    prompt,
+export async function generateChatResponce({
+  prompt,
+  history,
+}: {
+  prompt: string;
+  history: {
+    role: "user" | "model";
+    parts: [
+      {
+        text: string;
+      }
+    ];
+  }[];
+}) {
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+  const model = genAI.getGenerativeModel({
+    model: "models/gemini-1.5-flash-latest",
   });
 
-  return text;
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 1000,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history,
+  });
+
+  const result = await chat.sendMessage(prompt);
+  return result.response.text();
 }
